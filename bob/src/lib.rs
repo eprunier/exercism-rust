fac6c711a9210969c5609f1026eb1768b6458c2e
Eric Prunier <eric.prunier@gmail.com>
2017-06-02 18:16:45 +0200

    Bob solution #2

--- bob/src/lib.rs
+++ bob/src/lib.rs
@@ -1,15 +1,11 @@
 pub fn reply(input: &str) -> &str {
-	let result;
-
 	if input.is_empty() {
-		result = "Fine. Be that way!";
+		"Fine. Be that way!"
 	} else if input.to_uppercase() == input && input.to_lowercase() != input {
-		result = "Whoa, chill out!";
+		"Whoa, chill out!"
 	} else if input.ends_with("?") {
-		result = "Sure.";
+		"Sure."
 	} else {
-		result = "Whatever.";
+		"Whatever."
 	}
-
-	result
 }
